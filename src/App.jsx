601758463e6ebf5ca178c6997a7b89e56c860313
Joshua Chavez <jnchavezc@gmail.com>
2026-05-20 15:00:00 -0500

    fix: corregir las rutas de importación de los componentes ChatbotPage e InformacionNutricional

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Header from "./components/header";
 import MenuGrid from "./components/menu-grid";
 import Footer from "./components/footer";
-import ChatbotPage from "./pages/chatbot-page";
-import InformacionNutricional from "./pages/informacion-nutricional-page";
+import ChatbotPage from "./Pages/chatbot-page";
+import InformacionNutricional from "./Pages/informacion-nutricional-page";
 
 import './App.css'
 
