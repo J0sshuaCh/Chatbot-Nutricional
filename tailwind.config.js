e368cef1315887c16162867999e3149af2cb29a1
Aaronbejar <harian.bejar@unmsm.edu.pe>
2026-06-29 00:49:17 -0500

    refactor: optimizar configuración de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import containerQueries from '@tailwindcss/container-queries';
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    containerQueries
+    // ...
+  ],
 };
