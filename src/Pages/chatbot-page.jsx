f495da55be0b162e3e1f3878f5c6a88afb727b80
Joshua Chavez <jnchavezc@gmail.com>
2026-07-09 01:52:30 -0500

    fix: add min-h-0 class to CardContent for improved layout

--- src/Pages/chatbot-page.jsx
+++ src/Pages/chatbot-page.jsx
@@ -94,7 +94,7 @@ export default function ChatbotPage() {
               <CardTitle>Asistente Virtual</CardTitle>
             </div>
           </CardHeader>
-          <CardContent className="flex-1 flex flex-col p-0">
+          <CardContent className="flex-1 flex flex-col min-h-0 p-0">
             <div className="flex-1 overflow-y-auto min-h-0 px-4 py-4">
               <div className="flex flex-col gap-4">
                 {messages.map((msg) => (
